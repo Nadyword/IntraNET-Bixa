838fe1d3bb8de936640663bdee6a87a49ab041a3
Nadyword <83626464+Nadyword@users.noreply.github.com>
2026-06-17 18:46:12 -0400

    Update AppDbContextFactory.cs

--- backend/Bixa.Backend/Base/AppDbContextFactory.cs
+++ backend/Bixa.Backend/Base/AppDbContextFactory.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.EntityFrameworkCore;
 using Bixa.Backend.DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bixa.Backend.Base;
 
@@ -8,9 +8,12 @@ public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
 {
     public AppDbContext CreateDbContext(string[] args)
     {
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+
         var configuration = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
             .AddEnvironmentVariables()
             .Build();
 
