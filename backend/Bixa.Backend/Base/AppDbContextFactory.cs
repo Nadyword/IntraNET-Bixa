3252c94ff82c852c9fce036db66f21e99b3c3999
Nadyword <83626464+Nadyword@users.noreply.github.com>
2026-06-17 18:39:17 -0400

    Update AppDbContextFactory.cs

--- backend/Bixa.Backend/Base/AppDbContextFactory.cs
+++ backend/Bixa.Backend/Base/AppDbContextFactory.cs
@@ -10,7 +10,7 @@ public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
     {
         var configuration = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: true)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
             .AddEnvironmentVariables()
             .Build();
 
